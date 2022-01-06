8caaf6c8c392f5022d6080f3683984db6a79164d
Derek Schuff <dschuff@chromium.org>
2022-01-06 08:19:50 -0800

    Beginning of support for data symbols
    This is somewhat limited because local symbols may be a substantial portion
    of the data section.
    Also, currently bloaty includes space taken by a symbol's name in the
    name section in its attribution for the symbol (which can be larger
    than its space in the data section). This seems WAI for bloaty
    but might not be what we want.

--- lib_bloat.py
+++ lib_bloat.py
@@ -12,6 +12,7 @@
 # final linked output unless that object is the only one defining the symbol.
 
 from collections import namedtuple
+import operator
 import os
 from pathlib import Path
 import subprocess
@@ -41,11 +42,11 @@ def run_tool(cmd):
     return result_text
 
 
-def GetFuncSizes(wasm):
+def GetSymSizes(wasm):
     bloaty = BLOATY_DIR / 'bloaty'
     bloaty_output = run_tool([bloaty, '-d', 'symbols', '-n', '0',
                               '--demangle=none', '--csv', wasm])
-    func_sizes = {}
+    sym_sizes = {}
     total_size = 0
     for line in bloaty_output.split('\n'):
         #print(line)
@@ -55,26 +56,23 @@ def GetFuncSizes(wasm):
         #print(line)
         name, vmsize, filesize = line.split(',')
         total_size += int(filesize)
-        func_sizes[name] = int(filesize)
+        sym_sizes[name] = int(filesize)
 
-    print(f'{len(func_sizes)} functions in {wasm} ({total_size:,} bytes)')
-    return func_sizes
-
-
-def PrintLibSize(lib, size, weak, total):
-    percent = size / total * 100
-    print(f'Total lib size: {size:,} of {total:,} bytes ({percent:.1f}%)')
-    percent = (size + weak) / total * 100
-    print(f'Total lib size (including weak): {size + weak:,} of {total:,} bytes ({percent:.1f}%)')
+    print(f'{len(sym_sizes)} symbols in {wasm} ({total_size:,} bytes)')
+    return sym_sizes
 
 
 def GetLibFunctions(libs):
     nm = LLVM_DIR / 'llvm-nm'
     func_names = set()
     weak_names = set()
+    data_names = {}
+    local_data_names = set()
     for lib in libs:
         functions = 0
         weaks = 0
+        datas = 0
+        local_datas = 0
         nm_output = run_tool([nm, lib])
         for line in nm_output.split('\n'):
             try:
@@ -86,43 +84,77 @@ def GetLibFunctions(libs):
                 elif symtype.lower() == 'w': # A global or local weak symbol
                     weak_names.add(name)
                     weaks += 1
+                elif symtype.lower() == 'd':
+                    #if name in data_names and not name.startswith('.L') and  not 'piecewise_construct' in name:
+                    #    print(f'Warning: duplicate data name {name} in {lib} and {data_names[name]}')
+                    if name.startswith('.L'):
+                        local_data_names.add(name)
+                        local_datas += 1
+                    else:
+                        data_names[name] = lib
+                        datas += 1
             except ValueError: # fewer than 3 tokens
                 continue
         if VERBOSE:
-            print(f'{functions} functions and {weaks} weak symbols in {lib}')
-    return func_names, weak_names
+            print(f'{functions} functions, {weaks} weak symbols, and {data} data symbols in {lib}')
+    return func_names, weak_names, data_names, local_data_names
 
 
-LibSize = namedtuple('LibSize', ['name', 'function', 'weak', ])
+LibSize = namedtuple('LibSize', ['name', 'function', 'weak', 'data', 'local'])
 
-def GetLibSize(libs, func_sizes):
+def GetLibSize(libs, sym_sizes):
     lib_size = 0
     lib_weak_size = 0
-    lib_funcs, lib_weaks = GetLibFunctions(libs)
-    for func, size in func_sizes.items():
-        if func in lib_funcs:
+    lib_data_size = 0
+    local_data_size = 0
+    lib_funcs, lib_weaks, lib_datas, lib_local_datas = GetLibFunctions(libs)
+    for sym, size in sym_sizes.items():
+        if sym in lib_funcs:
             lib_size += size
-        elif func in lib_weaks:
+        elif sym in lib_weaks:
             lib_weak_size += size
+        if sym.startswith('.rodata') or sym.startswith('.data'):
+            stripped_name = sym.removeprefix('.rodata.').removeprefix('.data.')
+            if stripped_name in lib_datas:
+                lib_data_size += size
+            elif stripped_name.startswith('.L'):
+                local_data_size += size
     name = libs[0].name if len(libs) == 1 else '(aggregate)'
-    return LibSize(name, lib_size, lib_weak_size)
+    return LibSize(name, lib_size, lib_weak_size, lib_data_size, local_data_size)
+
+
+def GetDataSize(sym_sizes):
+    data_sym_count = 0
+    data_size = 0
+    for name, size in sym_sizes.items():
+        if name == '.data' or name == '.rodata':
+            print(f'Warning: wasm file seems to have a single merged {name}'
+                  f'section of size {size}')
+        elif (name.startswith('.data')
+              or name.startswith('.rodata')
+              or name.startswith('.tdata')):
+            data_sym_count += 1
+            data_size += size
+    return data_sym_count, data_size
 
 def main(args):
     libs = [Path(f) for f in args[:-1]]
     linked_wasm = Path(args[-1])
 
-    func_sizes = GetFuncSizes(linked_wasm)
-    linked_func_size = sum(size for func, size in func_sizes.items())
-    #print(f'Total functions size in {linked_wasm}: {total_func_size:,}')
+    sym_sizes = GetSymSizes(linked_wasm)
+    linked_sym_size = sum(size for sym, size in sym_sizes.items())
+    data_sym_count, data_sym_size = GetDataSize(sym_sizes)
+    #print(f'Total symbols size in {linked_wasm}: {linked_sym_size:,}')
+    print(f'{data_sym_count} data symbols ({data_sym_size:,} bytes)')
 
     sizes = []
 
     for lib in libs:
-        sizes.append(GetLibSize([lib], func_sizes))
+        sizes.append(GetLibSize([lib], sym_sizes))
     sizes.sort(key=lambda i: i.function + i.weak, reverse=True)
 
     def Percent(s):
-        return s / linked_func_size * 100
+        return s / linked_sym_size * 100
 
     print(' ' * 62 + 'Functions' + ' ' * 4 + '(Functions + weak syms)')
     print(f'{"Name":50}' + '      size        pct       size        pct')
@@ -130,6 +162,13 @@ def main(args):
         print(f'{lib.name:50}{lib.function:10,}{Percent(lib.function):10.1f}%\t', end='')
         combined = lib.function + lib.weak
         print(f'{combined:10,}{Percent(combined):10.1f}%')
+
+
+    sizes.sort(key=operator.attrgetter('data'), reverse=True)
+    print(' ' * 62 + 'Data')
+    print(f'{"Name":50}' + '      size        pct (of all data)')
+    for lib in sizes:
+        print(f'{lib.name:50}{lib.data:10,}{lib.data / data_sym_size*100:10.1f}%')
 
 
     libs_size_sum = sum(lib.function for lib in sizes)
@@ -143,16 +182,19 @@ def main(args):
     # as this would result in a multiple definition error). Warn in this case.
     # Probably there is some inaccuracy in this script, or perhaps some object
     # file was generated but not actually included in the link.
-    deduped_size = GetLibSize(libs, func_sizes)
+    deduped_size = GetLibSize(libs, sym_sizes)
     if libs_size_sum != deduped_size.function:
         print(f'warning: sum of strong definition sizes from all inputs is {libs_size_sum}, deduplicated total is {deduped_size.function}')
     #assert total_size.function == libs_size
     deduped_weak_size = deduped_size.function + deduped_size.weak
     #libs_weak_size = sum(lib.function + lib.weak for lib in sizes)
+    libs_data_size = sum(lib.data for lib in sizes)
 
-    print(f'Total lib size: {deduped_size.function:,} of {linked_func_size:,} bytes ({Percent(deduped_size.function):.1f}%)')
-    print(f'Total lib size (including weak): {deduped_weak_size:,} of {linked_func_size:,} bytes ({Percent(deduped_weak_size):.1f}%)')
-
+    print(f'Total size covered by strong functions in libs: {deduped_size.function:,} of {linked_sym_size:,} bytes ({Percent(deduped_size.function):.1f}%)')
+    print(f'Total size covered by strong and weak functions in libs: {deduped_weak_size:,} of {linked_sym_size:,} bytes ({Percent(deduped_weak_size):.1f}%)')
+    print(f'Total size covered by public data in libs: {libs_data_size:,} of {data_sym_size:,} bytes ({Percent(libs_data_size):.1f}% of total symbols, {libs_data_size/data_sym_size* 100:.1f}% of data section)')
+    local_data = sizes[0].local
+    print(f'Total size covered by local data (not attributable to libs): {local_data:,} of {data_sym_size:,} bytes ({Percent(local_data):.1f}% of total symbols, {local_data/data_sym_size*100:.1f}% of data section)')
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
